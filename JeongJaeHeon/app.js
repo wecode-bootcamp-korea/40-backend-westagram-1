de889e124db41ba753403d8c25d5731da7705f0f
jeongjaeheon <jaeheonjason@gmail.com>
2022-11-23 21:34:27 +0900

    [40기 정재헌] Westagram Backend Project-유저 회원가입 하기 구현

---
+++ JeongJaeHeon/app.js
@@ -0,0 +1,58 @@
+const http = require('http');
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const { DataSource } = require('typeorm')
+
+const app = express();
+
+const myDataSource = new DataSource({
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+})
+
+myDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized")
+    })
+    .catch(() => {
+        console.log("Data Source has not been initialized")
+    })
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+app.get('/ping',(req, res, next) => {
+    res.status(200).json({message : 'pong'});
+});
+
+app.post('/users/create', async (req, res, next) => {
+    const{ userName, userEmail, userPassword, coverImage } = req.body
+
+    await myDataSource.query(
+        `INSERT INTO users (
+            user_name, 
+            user_email, 
+            user_password, 
+            cover_image
+        ) VALUES (?, ?, ?, ?);
+        `,
+          [userName, userEmail, userPassword, coverImage]
+    ) 
+    res.status(201).json({ message : "userCreated"})
+});
+
+const PORT = process.env.PORT;
+
+const start = async() => {
+    app.listen(PORT, () => console.log(`Server is listening to ${PORT}`));
+}
+
+start();
