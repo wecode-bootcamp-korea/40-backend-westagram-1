4c57378703c34435d37cea8b6d9241047f56ad53
HyungJoonYoon <hysy9255@gmail.com>
2022-11-26 20:57:56 +0900

    likes.sql에 unique constraint 추가 완료

--- hyungjoonYoon/db/migrations/20221122152121_likes.sql
+++ hyungjoonYoon/db/migrations/20221122152121_likes.sql
@@ -3,6 +3,7 @@ CREATE TABLE likes (
   id INT NOT NULL, 
   user_id INT NOT NULL,
   post_id INT NOT NULL,
+  CONSTRAINT user_post_combination_ukey UNIQUE (user_id, post_id),
   PRIMARY KEY(id),
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (post_id) REFERENCES posts(id)
